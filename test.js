131b2da982f04500235a2e7ec9fd926b966c0331
Cyborg <83394128+Majidul17068@users.noreply.github.com>
2022-11-30 16:03:28 +0600

    Update

--- test.js
+++ test.js
@@ -1,10 +1,26 @@
-const book={
-    title: "Big Bang !",
-    author: "murad"
-}
-const bookJSN = JSON. stringify(book)
+// const fs = require('fs')
+// // const book={
+// //     title: "Big Bang !",
+// //     author: "murad"
+// // }
+// // const bookJSN = JSON. stringify(book)
+// // console.log(bookJSN)
+// // const bookData = JSON.parse(bookJSN)
+// // console.log(bookData.author)
+// //
+// // fs.writeFileSync('test-json.JSON',bookJSN)
+// const buffer = fs.readFileSync('test-json.JSON')
+// const dataJSN = buffer.toString()
+// //console.log(buffer.toString())
+// const data = JSON.parse(dataJSN)
+// console.log(data.title)
+//Test Json: access those by json
+const fs = require('fs')
+const testBuffer = fs.readFileSync('test-json.JSON')
+const testJSN = testBuffer.toString()
+const data = JSON.parse(testJSN) //to convert text into a JavaScript object
+data.name ='murad'
+const usrdata = JSON.stringify(data)
+fs.writeFileSync('test-json.JSON',usrdata)
+console.log(data.name)
 
-console.log(bookJSN)
-
-const bookData = JSON.parse(bookJSN)
-console.log(bookData.author)
